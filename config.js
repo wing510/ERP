3d1b4347666bc4754b9e0d8df1e3ecbe22a5c4a7
wing510 <s910374@gmail.com>
2026-04-20 19:39:17 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * ERP 前端設定（部署時主要改 API_BASE 即可）
+ * 若需在「不修改本檔」下覆寫：在 index.html 於本檔之前執行
+ *   window.__ERP_CONFIG__ = { API_BASE: "https://你的部署網址/exec" };
+ */
+(function () {
+  var defaults = {
+    API_BASE:
+      "https://script.google.com/macros/s/AKfycbw9t4s4n0NUohONMCP6Wu75WXZL1WXLu_wLBf2myxOepLuDEFwJc72RpTS_LXeSA32Q/exec"
+  };
+  var prev = typeof window.__ERP_CONFIG__ === "object" && window.__ERP_CONFIG__ !== null ? window.__ERP_CONFIG__ : {};
+  window.__ERP_CONFIG__ = Object.assign({}, defaults, prev);
+})();
