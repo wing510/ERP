6f1ac4f1c9c0668da02ae21fb05ba9cb7f9325a3
wing510 <s910374@gmail.com>
2026-04-24 16:46:29 +0800

    Add files via upload

--- js/core/config.js
+++ js/core/config.js
@@ -6,7 +6,7 @@
 (function () {
   var defaults = {
     API_BASE:
-      "https://script.google.com/macros/s/AKfycbwLMBavqx9HhY8PMm_cgH40VbceLWCpxgG0pmKUyO7fkN35o66COjLVGvaLnNST4Ywj/exec" // prev: AKfycby0l_M5Hn9xL5V6Jb2Cyx8_Gg-2iF_u9F3TYaLzwmTjiuqMPiGuoEdyil2UvKrFCSDd
+      "https://script.google.com/macros/s/AKfycbyH48I4fmxHCFm6DLIw4KHJRt7jI-dPJHrIcJs8WM0P_qJiMFcm4PYRdsB_MiiG5xFl/exec" // prev: AKfycbyLGwoHl6N0nsQX77bTTxvJXz-lCwHWVT7BfcfEktjEpDUvwGUdNn4e79aAEk1-DyMt
     ,
     // Google Sign-In（GIS）Client ID（Web）
     // - PROD：GitHub Pages
